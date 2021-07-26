999eac86c7b854590432058a99aa2ec262555d4b
MKen212 <46496040+MKen212@users.noreply.github.com>
2021-07-26 14:57:40 +0200

    Initial Setup & Getting Started

---
+++ browser/script-01.js
@@ -0,0 +1,42 @@
+"use strict";
+
+// eslint-disable-next-line no-undef
+const mainnet = new ripple.RippleAPI({
+  server: 'wss://s1.ripple.com'
+});
+
+// Get Ledger Info from Mainnet
+(async function(api) {
+  await api.connect();
+
+  let response = await api.getLedger({
+    includeTransactions: true
+  });
+  console.log(response);
+
+  // Convert Total Drops to XRP
+  console.log("Total XRP: " + api.dropsToXrp(response.totalDrops));
+
+})(mainnet);
+
+// eslint-disable-next-line no-undef
+const testnet = new ripple.RippleAPI({
+  server: "wss://s.altnet.rippletest.net/"
+});
+
+// Get Ledger Info from Testnet
+(async function(api) {
+  await api.connect();
+  let response = await api.getLedger({
+    includeTransactions: true
+  });
+  console.log(response);
+})(testnet);
+
+// Get Transaction Info
+(async function(api) {
+  await api.connect();
+  let response = await api.getTransaction("01AAEF23D5190D2413843CADD489AAB9C85D08D56D8A27CD7837291F96955D8A"
+  );
+  console.log(response);
+})(mainnet);
